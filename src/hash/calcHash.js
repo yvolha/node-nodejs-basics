1dea1695252f2dddceee4791d2ee33b89ffc8625
Olya Y <f_akora@mail.ru>
2023-06-19 02:18:28 +0300

    feat: complete hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+const { createHash } = await import('node:crypto');
+const hash = createHash('sha256');
+
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const toHash = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const contents = await readFile(toHash);
+    hash.update(contents);
+    process.stdout.write(hash.digest('hex'));
 };
 
 await calculateHash();
