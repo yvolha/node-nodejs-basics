14ef71504fc49a5a643149af059e139cf4a15dc3
Olya Y <f_akora@mail.ru>
2023-06-18 23:42:05 +0300

    feat: complete fs-read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { dirname, join} from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'fs/promises';
+import { stdout } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const writeable = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const contents = await readFile(writeable)
+    .catch(() => {
+        throw new Error('FS operation failed');
+    });
+    
+    stdout.write(contents);
 };
 
 await read();
