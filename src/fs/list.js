9f15e956b5ea3f7e0330a3f63bcaeacc98d92d61
Olya Y <f_akora@mail.ru>
2023-06-18 23:21:34 +0300

    feat: complete fs-list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { join, dirname} from 'path';
+import { fileURLToPath } from 'url';
+import { readdir } from 'fs/promises';
+import { stdout } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const baseDir = join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+    const names = await readdir(baseDir).catch(() => {
+        throw new Error('FS operation failed');
+    });
+    for (const name of names) {
+        stdout.write(name + '\n');
+    }
 };
 
 await list();
