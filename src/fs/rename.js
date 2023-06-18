75ca1309660da703867a7538bb4eeaeafacc7816
Olya Y <f_akora@mail.ru>
2023-06-18 21:32:19 +0300

    feat: complete fs-rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { access, constants } from 'node:fs/promises';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const wrongName = join(__dirname, 'files', 'wrongFilename.txt');
+const correctName = join(__dirname, 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+    const isFile = await access(correctName, constants.F_OK).then(() => true).catch(() => false);
+    if (isFile) throw new Error('FS operation failed');
+
+    fs.rename(wrongName, correctName, (err) => {
+        if (err) throw new Error('FS operation failed');
+    })
 };
 
 await rename();
