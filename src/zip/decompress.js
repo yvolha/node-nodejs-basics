24535fe832558e8d76558acec60d345eebca06c7
Olya Y <f_akora@mail.ru>
2023-06-20 00:26:07 +0300

    feat: complete zip-decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream} from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileToDecompress = join(__dirname, 'files', 'archive.gz');
+const decompressedFile = join(__dirname, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    const gunzip = createGunzip();
+    const sourceStream = createReadStream(fileToDecompress);
+    const resultStream = createWriteStream(decompressedFile);
+
+    pipeline(sourceStream, gunzip, resultStream, (err) => {
+        if (err) {
+          process.stdout.write('An error occurred:', err);
+        }
+      });
 };
 
 await decompress();
