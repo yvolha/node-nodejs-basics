66675fc720ff39f9ae1a46fee63589993f6538c4
Olya Y <f_akora@mail.ru>
2023-06-19 23:22:27 +0300

    feat: complete streams-write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createWriteStream } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const baseFile = join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    process.stdout.write("Please type something to save it in './files/fileToWrite.txt' \n");
+    const writeStream = createWriteStream(baseFile);
+
+    process.stdin.pipe(writeStream);
+
+    process.on('SIGINT', () => {
+        writeStream.end();
+        process.stdout.write("Thank you for typing with us!");
+        process.exit();
+    });
 };
 
 await write();
