dd0a6c0543eccd84f503c5fc3b6873857bbb3e86
Olya Y <f_akora@mail.ru>
2023-06-20 02:48:23 +0300

    feat: complete child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { fork } from 'child_process';
+import { join, dirname} from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const scriptFile = join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork(scriptFile, args, { stdio: ['inherit', 'inherit', 'inherit', 'ipc']});
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['greenArg1', 'yellowArg2', 'redArg3']);
