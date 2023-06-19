b00e1f6b4a4409530e86bc19145290cb5db94f71
Olya Y <f_akora@mail.ru>
2023-06-20 00:18:24 +0300

    feat: complete zip-compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+import { createReadStream, createWriteStream} from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileToCompress = join(__dirname, 'files', 'fileToCompress.txt');
+const compressedFile = join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const sourceStream = createReadStream(fileToCompress);
+    const resultStream = createWriteStream(compressedFile);
+
+    pipeline(sourceStream, gzip, resultStream, (err) => {
+        if (err) {
+          process.stdout.write('An error occurred:', err);
+        }
+      });
 };
 
 await compress();
